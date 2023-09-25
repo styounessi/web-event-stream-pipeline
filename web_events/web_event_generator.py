5e585e66ff09826424abf222a28fa93ae1dd34a0
Steve Younessi <63491724+styounessi@users.noreply.github.com>
2023-09-24 21:32:12 -0700

    Formatting clean up

--- web_events/web_event_generator.py
+++ web_events/web_event_generator.py
@@ -1,5 +1,6 @@
 import os
 import json
+
 from kafka import KafkaProducer
 from fake_web_events import Simulation
 
@@ -10,8 +11,10 @@ RAW_WEB_EVENTS = os.getenv('OUTPUT_TOPIC')
 
 
 # Initialize a Kafka producer and configure JSON value serializer
-producer = KafkaProducer(bootstrap_servers=BROKER,
-                         value_serializer=lambda x: json.dumps(x).encode('utf-8'))
+producer = KafkaProducer(
+    bootstrap_servers=BROKER,
+    value_serializer=lambda x: json.dumps(x).encode('utf-8')
+)
 
 
 def event_simulator(producer, output_topic):
@@ -19,6 +22,7 @@ def event_simulator(producer, output_topic):
     Simulate web events and send them to a Kafka topic.
     '''
     web_event_sims = Simulation(user_pool_size=50, sessions_per_day=500)
+    
     for event in web_event_sims.run(duration_seconds=999):
         producer.send(output_topic, value=event)
         print(event) # Print the simulated events for debugging purposes
