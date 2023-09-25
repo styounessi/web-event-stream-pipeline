4a23aa8389b51ac3dd5976a7b81a04a47753fd3a
Steve Younessi <63491724+styounessi@users.noreply.github.com>
2023-09-24 21:26:19 -0700

    Add user_email_domain event enrichment

--- faust_processor/app.py
+++ faust_processor/app.py
@@ -1,6 +1,8 @@
 import os
 import faust
 
+from event_ops import EventEnrichment
+
 
 # Grab Kafka broker and input/output topic environment variables
 BROKER = os.getenv('KAFKA_BROKER')
@@ -20,38 +22,21 @@ input_topic = app.topic(RAW_WEB_EVENTS)
 output_topic = app.topic(ENRICHED_WEB_EVENTS)
 
 
-# Class to enrich and/or manipulate web events
-class EventEnrichment:
-    @staticmethod
-    def categorize_utm_source(utm_source):
-        '''
-        Categorizes UTM (Urchin Tracking Modules) sources
-        into higher level groupings.
-
-        Args:
-            utm_source (str): UTM source value.
-
-        Returns:
-            str: Assigned category of the UTM source.
-        '''
-        if utm_source in ['google', 'bing']:
-            return 'search_engine'
-        elif utm_source in ['facebook', 'instagram']:
-            return 'social_media'
-        elif utm_source in ['mailchimp']:
-            return 'email_marketing'
-        else:
-            return 'other/unknown'
-
-
-# Faust agent to categorize UTM sources and send events to the output topic
+# Faust agent to categorize UTM sources, extract user email domains, and
+# send enriched events to output topic
 @app.agent(input_topic)
-async def categorize_utm_source_agent(events):
+async def enrichment_agent(events):
     async for event in events:
         utm_source = event.get('utm_source')
         if utm_source:
             source_category = EventEnrichment.categorize_utm_source(utm_source)
             event['source_category'] = source_category
+        
+        user_custom_id = event.get('user_custom_id')
+        if user_custom_id:
+            email_domain = EventEnrichment.extract_email_domain(user_custom_id)
+            event['user_email_domain'] = email_domain
+        
         await output_topic.send(value=event)
 
 
